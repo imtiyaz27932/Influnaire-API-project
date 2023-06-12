2934929c30f3dea6ef6185d1912b1140203cc650
imtiyaz27932 <imtiyaz.gw@gmail.com>
2023-06-12 09:27:29 +0530

    Add sms_resendOTP, login schema and register schema

---
+++ cypress/e2e/SMS/sms_resendOTP.cy.js
@@ -0,0 +1,54 @@
+
+
+describe('API Tests', () => {
+  it('SMS resend OTP API functionality', () => {
+    cy.fixture('SMS/resend.json').then((data) => {
+      const requestBody = data.validPhone
+      console.log('requestBody: ', requestBody);
+
+      cy.request({
+        method: 'POST',
+        url: "https://api.influnaire.dreamkashmir.com/api/v1/sms/resend-otp",
+        body: requestBody,
+        failOnStatusCode: false
+      })
+        .then((response) => {
+          try {
+            cy.log("response: ", response);
+            const userDetails = `OTP is: ${response.body}`
+            cy.log("User Details:", userDetails);
+            expect(response.status).to.equal(200);
+          } catch (error) {
+            cy.log('AssertionError:', error.message);
+          }
+        });
+    });
+  });
+
+  it('SMS verify OTP API functionality', () => {
+    cy.fixture('SMS/resend.json').then((data) => {
+      const requestBody = data.verifyotp;
+      console.log('requestBody: ', requestBody);
+
+      cy.request({
+        method: 'POST',
+        url: "https://api.influnaire.dreamkashmir.com/api/v1/sms/verify-otp",
+        body: requestBody,
+        failOnStatusCode: false
+      })
+        .then((response) => {
+          try {
+            cy.log("response: ", response);
+            const userDetails = `Message is: ${response.body.error.message}`
+            cy.log("User Details:", userDetails);
+            expect(response.status).to.equal(400);
+            expect(response.body.error.message).to.equal('invalid OTP')
+          } catch (error) {
+            cy.log('AssertionError:', error.message);
+          }
+
+        });
+    })
+  })
+})
+
